5f47e3ffbb560b76d4ae8826745c4ab1ceb2f39f
Johnakn <akannijhn@gmail.com>
2026-03-07 18:11:00 +0100

    Update backend entry2 point for deployment

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -19,10 +19,7 @@ const PORT = Number(process.env.PORT) || 4000;
 // Middleware
 app.use(
   cors({
-    origin:
-      process.env.FRONTEND_URL ||
-      "http://localhost:3000" ||
-      "https://abbeyfrontend.onrender.com",
+    origin: process.env.FRONTEND_URL || "https://abbeyfrontend.onrender.com",
     credentials: true,
   }),
 );
